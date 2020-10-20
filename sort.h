d13a7ed1e00fff3e8ac95464d803e9a4841643c5
Valentinaga1 <1881@holbertonschool.com>
2020-10-20 16:47:06 +0000

    Header file

--- sort.h
+++ sort.h
@@ -13,9 +13,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_array(const int *array, size_t size);
@@ -28,4 +28,4 @@ void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void counting_sort(int *array, size_t size);
 
-#endif 
+#endif
